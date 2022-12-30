1cb53cb2d8ca5e68b536deb4707991beee468457
Doina <gherasimdoina93@gmail.com>
2022-12-30 17:51:33 +0200

    calculator app - calculate result (+ - / x )

--- src/components/JAVASCRIPT/JavaScriptPractice.js
+++ src/components/JAVASCRIPT/JavaScriptPractice.js
@@ -1,81 +1,22 @@
-import classes from "./JavaScriptPractice.module.css";
 import {React, useState} from "react";
 
 function JavaScriptPractice () {
-    // question 1
-    // const [password, setPassword] = useState("");
-    // const [passwordLength, setPasswordLength] = useState(8);
-  
-    // const generatePassword = () => {
-    //   let pwd = "";
-    //   for (let i = 0; i < passwordLength; i++) {
-    //     const charType = Math.floor(Math.random() * 4);
-    //     if (charType === 0) {
-    //       pwd += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    //     } else if (charType === 1) {
-    //       pwd += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    //     } else if (charType === 2) {
-    //       pwd += Math.floor(Math.random() * 10);
-    //     } else {
-    //       const specialChars = "!@#$%^&*()";
-    //       pwd += specialChars[Math.floor(Math.random() * specialChars.length)];
-    //     }
-    //   }
-    //   setPassword(pwd);
-    // };
+  const [myArray, setMyArray] = useState([1, 2, 3, 4, 5]);
+  const [counter, setCounter] = useState(0);
 
-    // part 2
-
-    // const [passwordLength, setPasswordLength] = useState(0);
-    // const [password, setPassword] = useState("");
-  
-    // const characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-  
-    // const generatePassword = (event) => {
-    //   event.preventDefault();
-    //   let newPassword = "";
-    //   for (let i = 0; i < passwordLength; i++) {
-    //     newPassword += characterList[Math.floor(Math.random() * characterList.length)];
-    //   }
-    //   setPassword(newPassword);
-    // };
-  
-    // PART 3
-    const [value, setValue] = useState(50);
-
+  const handleClick = () => {
+    if (myArray.length > 0) {
+      const newArray = [...myArray];
+      newArray.splice(counter, 1);
+      setMyArray(newArray);
+      setCounter((counter + 1) % myArray.length);
+    }
+  };
+   
     return (
       <div>
-        {/* <form>
-          <label htmlFor="password-length">Password Length:</label>
-          <input
-            type="number"
-            id="password-length"
-            value={passwordLength}
-            onChange={e => setPasswordLength(e.target.value)}
-          />
-          <button type="button" onClick={generatePassword}>Generate Password</button>
-        </form>
-        <p>{password}</p> */}
-        {/* <p>A DOUA</p>
-        <form onSubmit={generatePassword}>
-        <label htmlFor="passwordLength">Password Length:</label>
-        <input
-          type="number"
-          id="passwordLength"
-          value={passwordLength}
-          onChange={(event) => setPasswordLength(event.target.value)}
-        />
-        <button type="submit">Generate Password</button>
-      </form>
-      {password && <p>{password}</p>} */}
-       <input
-        type="range"
-        min="0"
-        max="100"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
-      <span>{value}</span>
+         <button onClick={handleClick}>Delete element</button>
+      <p>Elements in array: {myArray.join(", ")}</p>
       </div>
     );
 };
--- src/components/React/Calculator/Calculator.js
+++ src/components/React/Calculator/Calculator.js
@@ -3,28 +3,74 @@ import classes from "./calculator.css";
 import {useState} from "react";
 
 function Calculator() {
-    // 1. default values
-
+    // 1. Declare state variables
     const otherOperators = ["DEL","C", "+/-", "%"];
-    const digits =[ 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, ","];
+    const digits =[ 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, ".","="];
     const operators = ["/","x","-","+"];
     const [value, setValue] = useState([]);
-    const [displayValue, setDisplayValue] = useState([0]);
-    const [result, setResult] = useState(0);
+    const [firstNumber, setFirstNumber] = useState([]);
+    const [secondNumber, setSecondNumber] = useState([]);
+    const [operator, setOperator] = useState(null);
+    // const [result, setResult] = useState(0);
 
     // 2. clickHndler
     const clickHandler = (digit) => {
-        let currentValue = [...value, digit];
-        setValue(currentValue);
-        console.log("value",currentValue);
+     setValue([...value, digit]);
+     if(digit === "C") {
+        setValue([]);
+        return;
+     }
+     if (digit === "DEL") {
+        setValue(value.slice(0,-1));
+     }
+    //  if (digit === "%") {
+    //     const percent = firstNumber / 100;
+    //     setValue(percent);
+    //     console.log("value", value);
+    //  }
+     if(digit === "/" || digit === "x" || digit === "+" || digit === "-" ) {
+        // set first part of the array
+        setFirstNumber(parseInt(value.join("")),10);
+        // set the oprator that was clicked
+        setOperator(digit);
+     }
+
+     if(digit === "=") {
+        // find the index of the used oprator => [2,3,4,"/",4,5]
+        const index = value.indexOf(operator);
+       // define the second part of the array, after the operator
+        const secondNr= parseInt(value.slice(index+1).join(""),10);
+        setSecondNumber(secondNr);
+       // calculate the result
+        let result;
+        switch (operator) {
+            case "+": 
+                result = firstNumber + secondNr;
+                break;
+            case "-":
+                result = firstNumber - secondNr;
+                break;
+            case "/":
+                result = firstNumber / secondNr;
+                break;
+            case "x":
+                result = firstNumber * secondNr;
+                break;              
+            default:
+                result = NaN;
+        }
+
+        console.log("result", result);
+        setValue([result]);
+        
+     }
     };
-    
 
    return (
         <div className="container">
             <div className="calculator_container">
                 <div className="screen_container">
-                    <p className="displayDigit">Ans is {displayValue}</p>
+                    <p className="displayDigit">Ans is {firstNumber}</p>
                     <p className="screen">{value}</p></div>
 
                 <div className="buttonBox buttonBoxGrey">{
@@ -40,7 +86,6 @@ function Calculator() {
                     <button className="button" key={index} onClick={()=>{clickHandler(element);}}>
                         <p className="text">{element}</p></button>
                     )}
-                    <button className="button" style={{backgroundColor:"#233EDC"}}>=</button>
                 </div>
                 <div className="buttonBox buttonBoxOperators">{
                     operators.map((element, index)=>
