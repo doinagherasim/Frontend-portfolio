5ab55b40c977e0e46979e8bea9da049768dfa6f6
Doina <gherasimdoina93@gmail.com>
2023-01-16 20:25:42 +0200

    portofolio home page HTML CSS

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -1,18 +1,29 @@
-import frontend from "../../assets/frontend.png";
+import classes from "./homePage.module.css";
+import gmail from "../../assets/gmail.png";
 
 function HomePage() {
+
   return (
-    <div className="containe-home">
-        <div className="header">
-            <div>gmail</div>
-            <div>
-                <ul>
+    <div className={classes.containerHome}>
+        <div className={classes.header}>
+            <div className={classes.contact}>
+              <img className={classes.contactImg} src={gmail} width="40" height="40"/>
+              <a href="mailto:gherasimdoina93@gmail.com" className={classes.mail}> gherasimdoina93@gmail.com</a>
+            </div>
+            <div className={classes.menu}>
+                <ul className={classes.menu_ul}>
                     <li>About me</li>
                     <li>Resources</li>
                     <li>Certificates</li>
-                    <li>Projects</li>
+                    <li><a className={classes.link} href="/allprojects"> All projects</a></li>
                 </ul>
             </div>
+        </div>
+        <div className={classes.container_center}>
+          <div className={classes.center}>
+              <h1 className={classes.name}>FRONTEND DEVELOPER</h1>
+          </div>
+        <button className={classes.viewprojects}><a className={classes.projects} href="/allprojects">View projects</a></button>
         </div>
     </div>
   );
