fc97eae425a0d5e54a801b71f4d652c3c26d9956
Doina <gherasimdoina93@gmail.com>
2023-01-31 15:26:27 +0200

    portfolio-menu-responsive

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -103,44 +103,6 @@ responses;
             </div>
                     )
                 }
-            {/* <div className={classes.certificate}>
-                <img className={classes.zoom} src={htmlCertificate} width="90%"/>
-                <h3>HTML and CSS certificate</h3>
-                <button className={classes.btnDetails} onClick={detailsHandler}>View details</button>
-                <ul className={`${classes.details} ${showDetails ? classes.details : classes.hidden}`} >
-                    <li>- modern CSS, including flexbox and CSS Grid for layout</li>
-                    <li>- important CSS concepts such as the box model, positioning schemes, inheritance, solving selector conflicts, etc.</li>
-                    <li>- responsive design.</li>
-                </ul>
-            </div>
-            <div className={classes.certificate}>
-                <img className={classes.zoom} src={jsCertificate} width="90%"/>
-                <h3>JavaScript certificate</h3>
-                <button className={classes.btnDetails} onClick={detailsHandler}>View details</button>
-                <ul className={`${classes.details} ${showDetails ? classes.details : classes.hidden}`}>
-                    <li>- JavaScript fundamentals: variables, if/else, operators, boolean logic, functions, arrays, objects, loops, strings</li>
-                    <li>- arrow functions, destructuring, spread operator, default arguments</li>
-                    <li>- JavaScript works behind the scenes</li>
-                    <li>- object-oriented programming</li>
-                    <li>- the event loop, promises, async/await, and error handling</li>
-                    </ul>
-            </div>
-            <div className={classes.certificate}>
-                <img className={classes.zoom} src={reactCertificate} width="90%"/>
-                <h3>React certificate</h3>
-                <button className={classes.btnDetails} onClick={detailsHandler}>View details</button>
-                <ul className={`${classes.details} ${showDetails ? classes.details : classes.hidden}`}>
-                    <li>- core basics: How React works, building components with React & building UIs with React</li>
-                    <li>- components, props & dynamic data binding</li>
-                    <li>- user events and state.</li>
-                    <li>- react Hooks</li>
-                    <li>- sending Http requests & handling transitional states + responses</li>
-                    <li>- handling forms and user input, incl. validation</li>
-                    <li>- routing with React Router</li>
-                    <li>- combining React with TypeScript</li>
-                    <li>- handling forms and user input, incl. validation</li>
-                    </ul>
-            </div> */}
         </div>
         </div>
     );
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import classes from "./header.module.css";
 import gmail from "../../assets/gmail.png";
 import number from "../../assets/number.png";
 import linkedin from "../../assets/linkedin.png";
 import git from "../../assets/git.png";
+import menu from "../../assets/menu.png";
+import react from "react";
 
 function Header(){
+    const [close, setClose] = useState(false);
+    const closeHandler = () => {
+        setClose(false);
+    };
+    const showMenu = () => {
+        setClose(true);
+    };
+
     return(
       <div className={classes.header}>
+        <div className={classes.menuWrap}>
+            <img className={classes.menuBtn} src={menu} width="40" height="40" onClick={showMenu}/>
+            <p className={classes.menuText}>Menu</p>
+            </div>    
+            <div className={`${classes.menuList} ${close ? classes.menuList : classes.hidden}`}>
+                <button className={classes.close} onClick={closeHandler}>Close x</button>
+                    <button className={classes.linkM}><a href="/"><span className={classes.menuLink}>Home</span></a></button>
+                    <button className={classes.linkM}><a href="/certificates"><span className={classes.menuLink}>Certificates</span></a></button>
+                    <button className={classes.linkM}><a href="/allprojects/small_projects"><span className={classes.menuLink}>All projects</span></a></button>
+                    <button className={classes.linkM}><a href="/allprojects/small_projects"><span className={classes.menuLink}>Contacts</span></a></button>
+            </div>
+
         <div className={classes.contact}>
               <img className={classes.contactImg} src={gmail} width="25" height="25"/>
               <a href="mailto:gherasimdoina93@gmail.com" className={classes.mail}> gherasimdoina93@gmail.com</a>
