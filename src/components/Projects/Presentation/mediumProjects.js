ba018685e3c3cc2e37eca071397851cbcd0f4d5f
Doina <gherasimdoina93@gmail.com>
2023-03-13 09:22:12 +0200

    food-app-presentation-in-progress

--- src/components/Projects/Presentation/mediumProjects.js
+++ src/components/Projects/Presentation/mediumProjects.js
@@ -2,11 +2,12 @@ import AllProjects from "./projects";
 import classes from "./mediumProjects.module.css";
 import { useEffect, useRef } from "react";
 import videoCat from "../../../assets/videoCat.mp4";
+import videoFood from "../../../assets/foodOrderApp.mp4";
 
 
 function MediumProjects () {
     const videoRef = useRef();
-    useEffect (() => {videoRef.current.play();},[]);
+    // useEffect (() => {videoRef.current.play();},[]);
 
     return (
         <div>
@@ -23,7 +24,7 @@ function MediumProjects () {
                     <a className={classes.btn} href="https://cat-feed-app.web.app/" target="_blank">
                         <p className={classes.btnLink}>See how it works → </p>
                     </a>
-                    <a className={classes.btn} href="https://cat-feed-app.web.app/" target="_blank">
+                    <a className={classes.btn} href="https://github.com/doinagherasim/Feed-the-cat-app/tree/master/src" target="_blank">
                         <p className={classes.btnLink}>See GitHub → </p>
                     </a>  
                 </div>
@@ -31,19 +32,19 @@ function MediumProjects () {
             </div>
             <div className={classes.projectWrap}>
             <div className={classes.project_content} style={{textAlign:"right"}}>
-                    <h2 style={{color: "black"}}>ORDER FOOD APP</h2>
+                    <h2 style={{color: "black"}}>FOOD ORDER APP</h2>
                     <p className={classes.description}>Facilitates restaurant orders with menu selection, quantity adjustment, cost calculation, and delivery information input.</p>
                     <div className={classes.btn_wrap}>
-                    <a className={classes.btn} href="https://cat-feed-app.web.app/" target="_blank">
+                    <a className={classes.btn} href="https://food-order-app-c1611.web.app/" target="_blank">
                         <p className={classes.btnLink}>See how it works → </p>
                     </a>
-                    <a className={classes.btn} href="https://cat-feed-app.web.app/" target="_blank">
+                    <a className={classes.btn} href="https://github.com/doinagherasim/E-COMMERCE-FOOD-APP/tree/master/src" target="_blank">
                         <p className={classes.btnLink}>See GitHub → </p>
                     </a>  
                     </div>
                 </div>
                 <div className={classes.videoWrap}>
-                <video src={videoCat} ref={videoRef} muted autoPlay loop className={classes.video}/>
+                <video src={videoFood} ref={videoRef} muted autoPlay loop className={classes.video}/>
                 </div>
             </div>
             </div> 
